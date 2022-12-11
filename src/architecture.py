584ff928a6608b40a6aaa1e77fca1de21e985ae4
Novin <mnovinnam1997@gmail.com>
2022-12-11 20:59:53 +0330

    finish SaBlock - ConvBlock Layer : two * (convolution + group_norm + leaky_relu) - SaBlock Layer : ConvBlock * (AvgPool + ConvBlock + UpSample) + (AvgPool + ConvBlock + UpSample)

--- src/architecture.py
+++ src/architecture.py
@@ -1,9 +1,43 @@
 import tensorflow as tf
+import tensorflow_addons as tfa
+
+class ConvBlock(tf.keras.layers.Layer):
+    def __init__(self, filters):
+        super(ConvBlock, self).__init__()
+        self.first_conv = tf.keras.layers.Conv2D(filters, kernel_size=3, strides=(1, 1), padding="same", use_bias=True)
+        self.first_group_norm = tfa.layers.GroupNormalization(groups=32)
+        self.first_leaky_relu = tf.layers.LeakyReLU()
+        self.second_conv = tf.layers.Conv2D(filters, kernel_size=3, strides=(1, 1), padding="same", use_bias=True)
+        self.second_group_norm = tfa.layers.GroupNormalization(groups=32)
+        self.second_leaky_relu = tf.layers.LeakyReLU()
+
+    def call(self, input):
+        x = self.first_conv(input)
+        x = self.first_group_norm(x)
+        x = self.first_leaky_relu(x)
+        x = self.second_conv(x)
+        x = self.second_group_norm(x)
+        out = self.second_leaky_relu(x)
+        return out
 
 class SaBlock(tf.keras.layers.Layer):
-    def __init__(self, c):
+    def __init__(self, filters):
         super(SaBlock, self).__init__()
+        self.x_conv_block = ConvBlock(filters)
+        self.avg_pool = tf.keras.layers.AveragePooling2D()
+        self.y_conv_block = ConvBlock(filters)
+        self.up_sampling = tf.keras.layers.UpSampling2D()
+        self.multiply = tf.keras.layers.Multiply()
+        self.add = tf.keras.layers.Add()
 
-        self.conv_1 = tf.keras.layers.Conv2D(c, )
+    def call(self, input):
+        x = self.x_conv_block(input)
+        y = self.avg_pool(input)
+        y = self.y_conv_block(y)
+        y = self.up_sampling(y)
+        mul = self.multiply([x, y])
+        out = self.add([y, mul])
+        return out
 
-    def call(self, x):
+
+
