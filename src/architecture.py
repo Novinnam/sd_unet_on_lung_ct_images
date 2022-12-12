542ff5af08b1fef62b4aab9c4245cd0f5ffcf55a
Novin <mnovinnam1997@gmail.com>
2022-12-12 16:26:58 +0330

    Write DownConv and UpConv Blocks - DownConv: maxpool and avgpool + (convolution + group_norm + leaky_relu) - UpConv: up_sampling + (convolution + group_norm + leaky_relu)

--- src/architecture.py
+++ src/architecture.py
@@ -4,10 +4,10 @@ import tensorflow_addons as tfa
 class ConvBlock(tf.keras.layers.Layer):
     def __init__(self, filters):
         super(ConvBlock, self).__init__()
-        self.first_conv = tf.keras.layers.Conv2D(filters, kernel_size=3, strides=(1, 1), padding="same", use_bias=True)
+        self.first_conv = tf.keras.layers.Conv2D(filters, kernel_size=3, padding="same")
         self.first_group_norm = tfa.layers.GroupNormalization(groups=32)
         self.first_leaky_relu = tf.layers.LeakyReLU()
-        self.second_conv = tf.layers.Conv2D(filters, kernel_size=3, strides=(1, 1), padding="same", use_bias=True)
+        self.second_conv = tf.layers.Conv2D(filters, kernel_size=3, padding="same")
         self.second_group_norm = tfa.layers.GroupNormalization(groups=32)
         self.second_leaky_relu = tf.layers.LeakyReLU()
 
@@ -39,5 +39,34 @@ class SaBlock(tf.keras.layers.Layer):
         out = self.add([y, mul])
         return out
 
+class DownConv(tf.keras.layers.Layer):
+    def __init__(self, filters):
+        self.max_pool = tf.keras.layers.MaxPooling2D()
+        self.avg_pool = tf.keras.layers.AveragePooling2D()
+        self.concat = tf.keras.layers.Concatenate()
+        self.conv = tf.keras.layers.Conv2D(filters, kernel_size=1)
+        self.group_norm = tfa.layers.GroupNormalization(groups=32)
+        self.leaky_relu = tf.keras.layers.LeakyReLU()
+    
+    def call(self, input):
+        x = self.max_pool(input)
+        y = self.avg_pool(input)
+        z = self.concat([x, y])
+        z = self.conv(z)
+        z = self.group_norm(z)
+        out = self.leaky_relu(z)
+        return out
 
-
+class UpConv(tf.keras.layers.Layer):
+    def __init__(self, filters):
+        self.up_sampling = tf.keras.layers.UpSampling2D(interpolation='bilinear')
+        self.conv = tf.keras.layers.Conv2D(filters, kernel_size=1)
+        self.group_norm = tfa.layers.GroupNormalization(groups=32)
+        self.leaky_relu = tf.keras.layers.LeakyReLU()
+    
+    def call(self, input):
+        x = self.up_sampling(input)
+        x = self.conv(x)
+        x = self.group_norm(x)
+        out = self.leaky_relu(x)
+        return out
